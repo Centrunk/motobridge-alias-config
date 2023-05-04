707727347757a6632276c0a41e1c327c16839574
Darjeeling <carsten.gallini@gmail.com>
2023-05-03 20:29:36 -0500

    All mode compat

--- motobridge-alias-config/motobridge-alias-config/Form1.Designer.cs
+++ motobridge-alias-config/motobridge-alias-config/Form1.Designer.cs
@@ -28,8 +28,6 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
 			this.button3 = new System.Windows.Forms.Button();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
 			this.textBox1 = new System.Windows.Forms.TextBox();
@@ -46,33 +44,16 @@
 			this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
 			this.button7 = new System.Windows.Forms.Button();
 			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(346, 223);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(91, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "DMR";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(443, 223);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(91, 23);
-			this.button2.TabIndex = 1;
-			this.button2.Text = "TETRA";
-			this.button2.UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
 			this.button3.Location = new System.Drawing.Point(249, 223);
 			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(91, 23);
+			this.button3.Size = new System.Drawing.Size(120, 23);
 			this.button3.TabIndex = 2;
-			this.button3.Text = "P25";
+			this.button3.Text = "Generate";
 			this.button3.UseVisualStyleBackColor = true;
 			this.button3.Click += new System.EventHandler(this.button3_Click);
 			// 
@@ -143,7 +124,7 @@
 			// checkBox1
 			// 
 			this.checkBox1.AutoSize = true;
-			this.checkBox1.Location = new System.Drawing.Point(252, 198);
+			this.checkBox1.Location = new System.Drawing.Point(268, 198);
 			this.checkBox1.Name = "checkBox1";
 			this.checkBox1.Size = new System.Drawing.Size(88, 19);
 			this.checkBox1.TabIndex = 9;
@@ -199,11 +180,21 @@
 			this.label4.TabIndex = 14;
 			this.label4.Text = "Count";
 			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 279);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(361, 15);
+			this.label5.TabIndex = 15;
+			this.label5.Text = "This program works with P25, DMR, TETRA, and MCS2000 resources";
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.button7);
 			this.Controls.Add(this.label3);
@@ -217,8 +208,6 @@
 			this.Controls.Add(this.button4);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Rosesams Motobridge Alias Configurator ";
 			this.ResumeLayout(false);
@@ -227,9 +216,6 @@
 		}
 
 		#endregion
-
-		private Button button1;
-		private Button button2;
 		private Button button3;
 		private OpenFileDialog openFileDialog1;
 		private TextBox textBox1;
@@ -246,5 +232,6 @@
 		private OpenFileDialog openFileDialog2;
 		private Button button7;
 		private Label label4;
+		private Label label5;
 	}
 }
